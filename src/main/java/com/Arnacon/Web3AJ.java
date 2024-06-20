d4f69d24a11e2940f10479052680cb581f8d2471
datBatmanCoder <jonakandel@gmail.com>
2024-06-20 09:08:06 +0200

    1.0.10.1- changed name of get GSM number in app

--- src/main/java/com/Arnacon/Web3AJ.java
+++ src/main/java/com/Arnacon/Web3AJ.java
@@ -580,10 +580,6 @@ public class Web3AJ extends AWeb3AJ{
 
     public String getCalleeDomain(String callee) throws Exception {
 
-        if (callee.matches("\\d+")) {
-            return getCalleeDomainGSM(callee);
-        } 
-
         Web3j web3j = Web3j.build(new HttpService(this.network.getRPC()));
 
         HLUI contractHLUI = HLUI.load(
@@ -596,7 +592,11 @@ public class Web3AJ extends AWeb3AJ{
         return contractHLUI.getServiceProviderDomain(callee).send();
     }
 
-    private String getCalleeDomainGSM(String gsm) {
+    public String getGSMDomain(String gsm) {
+
+        if (!gsm.matches("\\d+")) {
+            throw new RuntimeException("Error: Invalid GSM");
+        } 
 
         String serviceProviderOfGsm = dataSaveHelper.getPreference(gsm, null);
         if (serviceProviderOfGsm == null){
